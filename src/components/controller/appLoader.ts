62a2b84984774aa50ba1447177d1c7de87a3b369
AlexJester147 <vardy2016@lenta.ru>
2021-12-04 14:09:35 +0300

    feat : install typescript

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'f2c49fb78eca4ed9ae89bc8c527a3d50', // получите свой ключ https://newsapi.org/
         });
     }
 }
